65f90bdefae4def203454520d167219a50cb9d1c
Nunie123 <ed.a.nunes@gmail.com>
2017-02-26 15:10:55 -0500

    WIP Updates to views and templates.

--- application/views.py
+++ application/views.py
@@ -3,4 +3,16 @@ from flask import render_template
 
 @application.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home', logged_in=1, first_name='Wilward', last_name='McNunes')
+
+@application.route('/orders')
+def orders():
+    return render_template('orders_all.html', title='Orders', logged_in=0, first_name='Wilward', last_name='McNunes')
+
+@application.route('/requests')
+def requests():
+    return render_template('requests_all.html', title='Requests', logged_in=1, first_name='Wilward', last_name='McNunes')
+
+@application.route('/users')
+def users():
+    return render_template('users.html', title='Users', logged_in=1, first_name='Wilward', last_name='McNunes')
