786c3917ed93e2ea91d50f68f312b843b058bda5
Nunie123 <ed.a.nunes@gmail.com>
2017-02-27 09:08:58 -0500

    Updated seed.py to populate more fields.

--- db_seed.py
+++ db_seed.py
@@ -1,16 +1,19 @@
 from application import db, models
 
-u1 = models.Users(first_name='First', last_name='User', email='first.user@spoons.online')
-u2 = models.Users(first_name='Second', last_name='User', email='second.user@spoons.online')
-u3 = models.Users(first_name='Third', last_name='User', email='third.user@spoons.online')
+u1 = models.Users(first_name='First', last_name='User', email='first.user@spoons.online', role='superuser')
+u2 = models.Users(first_name='Second', last_name='User', email='second.user@spoons.online', role='admin')
+u3 = models.Users(first_name='Third', last_name='User', email='third.user@spoons.online', role='requestor')
 
 p1 = models.Products(product_name='Spoons')
 p2 = models.Products(product_name='Forks')
 p3 = models.Products(product_name='Knives')
 
-r1 = models.Requests(requested_by=u1, product=p1)
-r2 = models.Requests(requested_by=u2, product=p2)
-r3 = models.Requests(requested_by=u3, product=p3)
+r1 = models.Requests(requested_by=u1, product=p1, status='pending')
+r2 = models.Requests(requested_by=u2, product=p2, status='pending', requestor_note="That ain't a knife.")
+r3 = models.Requests(requested_by=u3, product=p3, status='approved', reviewed_by=u1)
+r4 = models.Requests(requested_by=u1, product=p1, status='approved', reviewed_by=u1)
+r5 = models.Requests(requested_by=u2, product=p3, status='approved', requestor_note="That's a knife.", reviewed_by=u1)
+r6 = models.Requests(requested_by=u3, product=p2, status='approved', reviewed_by=u1)
 
 db.session.add(u1)
 db.session.add(u2)
